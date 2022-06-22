0240dac08dc1159c72188f8c6d2a670394a2a3e5
Sully Clark <clarkjs237@gmail.com>
2022-06-22 15:37:05 -0700

    no progress with chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+
 
 function App() {
 
@@ -17,22 +19,16 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
+  // return (
+  //   <ChakraProvider>
+  //     <div>
+  //       Hello
+  //     </div>
+  //   </ChakraProvider>
+  // )
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 // 1. import `ChakraProvider` component
+// import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
+// import ReactDOM, { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
-  // <ChakraProvider>
-  //   <App />
-  // </ChakraProvider>
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+
+// import * as React from 'react'
+// import ReactDOM from "react-dom/client";
+
+// // 1. import `ChakraProvider` component
+// import { ChakraProvider } from '@chakra-ui/react'
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <ChakraProvider>
+//     <App />
+//   </ChakraProvider>
+// )
+// // ReactDOM.render(
+//   // <ChakraProvider>
+//   //   <App />
+//   // </ChakraProvider>,
+// //   document.getElementById('root')
+// // );
+
+
+
+// // function Index() {
+// //   // 2. Wrap ChakraProvider at the root of your app
+// //   return (
+// //     <ChakraProvider>
+// //       <App />
+// //     </ChakraProvider>
+// //   )
+// // }
