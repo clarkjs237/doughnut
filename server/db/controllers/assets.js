7d660ee0316a4d1c8a785e215e5945bc837aad41
Sully Clark <clarkjs237@gmail.com>
2022-06-22 13:01:08 -0700

    finish db controllers

--- server/db/controllers/assets.js
+++ server/db/controllers/assets.js
@@ -2,29 +2,37 @@ const Assets = require('../models/assets.js');
 
 
 
-// This is where I will house my CRUD functions
+// This is where I will house my CRUD functions to my DB
 
-exports.getAll = () => {
-  // console.log('i am in controllers')
-  // const data = await Assets.find({});
-  // console.log(data)
-  // return data;
-  return Assets.find({});
+// Gets all of the entries from the database whose values aren't 0
+exports.getAll = async () => {
+  const data = await Assets.find({ amount : { $ne: 0}});
+  return data;
 }
 
-exports.addToDb = (input) => {
-  console.log(input);
-  // input is the object we want to add to the db
+exports.addToDb = async (input) => {
+  // This updates the input based on the name of the asset, like 'bitcoin' or 'ethereum'
+  // If it doesn't exist, it creates a new entry for this asset
+  const data = await Assets.findOneAndUpdate({name: input.name}, {...input}, {new: true, upsert: true});
 
-  // return Rsvp.findOneAndUpdate(
-  //   {
-  //     name: input.name,
-  //     class: input.class,
-  //     curr: input.currPrice,
-  //   }, { $set: input }, { upsert: true }
-  // )
-  // return Assets.findOneAndUpdate(input, { $set: input}, {upsert: true});
-
-  return Assets.findOneAndUpdate(input, { $set: input}, {upsert: true});
-
+  // Returns the updated input object. Might be unneccesary
+  return data;
 }
+
+
+// Don't actually delete from db, just set the amount to zero and filter out in frontend
+exports.deleteFromDb = async (input) => {
+  const data = await Assets.findOneAndUpdate({name: input}, {amount: 0}, {new: true});
+  return data;
+}
+
+
+
+// Instead of deleting from the db, let's just update the amount to be zero and filter
+// that out on the front end. This way I can still keep my urls and such
+// exports.deleteFromDb = async (input) => {
+//   // input will be the name of the thing we want to delete
+//   const data = await Assets.findOneAndDelete({name: input});
+
+//   return data;
+// }
--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,22 @@
 // const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 
+// This also works for establishing a connection and exporting it correctly
+const server = 'mongodb://localhost:27017';
+const database = 'assets';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${server}/${database}`)
+    console.log('Mongodb connected');
+  } catch (err) {
+    console.log('Failed to connect to mongodb')
+  }
+}
+
+module.exports = connectDB();
+
+
 
 // This right here works so far. I want to try and use async though.
 // const url = 'mongodb://localhost:27017/assets';
@@ -17,19 +33,3 @@ const mongoose = require('mongoose');
 // })
 
 // module.exports = db;
-
-
-// This also works for establishing a connection and exporting it correctly
-const server = 'mongodb://localhost:27017';
-const database = 'assets';
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(`${server}/${database}`)
-    console.log('Mongodb connected');
-  } catch (err) {
-    console.log('Failed to connect to mongodb')
-  }
-}
-
-module.exports = connectDB();
