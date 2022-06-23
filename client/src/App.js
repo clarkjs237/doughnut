fd442920638639ca3d9c9a7b982ae7301c04db44
Sully Clark <clarkjs237@gmail.com>
2022-06-22 20:34:03 -0700

    removed dev dependencies for react and it's working now

--- client/src/App.js
+++ client/src/App.js
@@ -27,37 +27,37 @@ function App() {
     setAssets(data);
   }, []);
 
-  if (assets !== null) {
-    console.log('im not null')
-    return (
-      <div>
-        <Heading>Rendezvous</Heading>
-        { assets !== null &&
-        <div>
-          <Accordion defaultIndex={[0]} allowMultiple>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex='1' textAlign='left'>
-                    Section 1 title
-                    <img src={assets[0].logo} alt="" />
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
-                </AccordionPanel>
-              </h2>
-            </AccordionItem>
-          </Accordion>
-        </div>}
-      </div>
+  // if (assets !== null) {
+  //   console.log('im not null')
+  //   return (
+  //     <div>
+  //       <Heading>Rendezvous</Heading>
+  //       { assets !== null &&
+  //       <div>
+  //         <Accordion defaultIndex={[0]} allowMultiple>
+  //           <AccordionItem>
+  //             <h2>
+  //               <AccordionButton>
+  //                 <Box flex='1' textAlign='left'>
+  //                   Section 1 title
+  //                   {/* <img src={assets[0].logo} alt="" /> */}
+  //                 </Box>
+  //                 <AccordionIcon />
+  //               </AccordionButton>
+  //               <AccordionPanel pb={4}>
+  //                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  //                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  //                 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  //                 commodo consequat.
+  //               </AccordionPanel>
+  //             </h2>
+  //           </AccordionItem>
+  //         </Accordion>
+  //       </div>}
+  //     </div>
 
-    );
-  }
+  //   );
+  // }
 
   return <div>Loading</div>
 
